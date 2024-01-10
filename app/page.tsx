feefaae8f918e640c3feda772d5f6932893023fd
selimdilsad <selimdilsadercan@gmail.com>
2024-01-10 17:16:34 +0300

    fix: moved toolbar to bottom

--- app/page.tsx
+++ app/page.tsx
@@ -55,11 +55,12 @@ function Page() {
   };
 
   return (
-    <main className=" h-[100vh] flex flex-col items-center  justify-between">
-      <header
-        className="mt-6 flex gap-6 w-[940px] p-5 fixed top-0 left-1/2 translate-x-[-50%]
-         z-10 bg-[#191919] rounded border border-[#3C3C3C] shadow-md"
-      >
+    <main className=" h-[100vh] flex flex-col items-center mt-8 justify-between">
+      <div className="code-editor-ref" ref={editorRef}>
+        <CodeEditor language={language} theme={theme} background={background} icon={activeIcon} currentPadding={currentPadding} />
+      </div>
+
+      <div className="mb-8 flex gap-6 w-[940px] p-5 fixed bottom-0 left-1/2 translate-x-[-50%] z-10 bg-[#191919] rounded border border-[#3C3C3C] shadow-md">
         <LanguageSelector language={language} setLanguage={setLanguage} seActiveIcon={setActiveIcon} />
         <ThemeSelector theme={theme} setTheme={setTheme} />
         <BackgroundSelector background={background} setBackground={setBackground} />
@@ -67,22 +68,14 @@ function Page() {
 
         <div className="export-btn self-center ml-auto">
           <button
-            className="flex items-center gap-3 py-2 px-3 bg-blue-400 rounded-md text-sm text-blue-400 
-              font-medium bg-opacity-10 hover:bg-opacity-80 hover:text-slate-50 ease-in-out transition-all 
-              duration-300"
+            className="flex items-center gap-3 py-2 px-3 bg-blue-400 rounded-md text-sm text-blue-400 font-medium bg-opacity-10 hover:bg-opacity-80 hover:text-slate-50 ease-in-out transition-all duration-300"
             onClick={exportPng}
           >
             <Download />
             Export PNG
           </button>
         </div>
-      </header>
-
-      {/* <div className="code-editor-ref mt-[14rem]" ref={editorRef}>
-        <CodeEditor language={language} theme={theme} background={background} icon={activeIcon} currentPadding={currentPadding} />
-      </div> */}
-
-      {/* <Footer /> */}
+      </div>
     </main>
   );
 }
